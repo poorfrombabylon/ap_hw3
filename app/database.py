5bfb487a63b32c858805e2bce1e08422d30dd983
poorfrombabylon <al06maz2002@gmail.com>
2025-03-26 10:16:12 +0300

    fix address

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:kek228@localhost:5432/postgres"
+DATABASE_URL = "postgresql://user:kek228@postgres:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
